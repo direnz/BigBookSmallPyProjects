7ddc7d9274d2cfa3da3de8526acdf0e964afa696
direnc88 <direnc88@users.noreply.github.com>
2023-06-29 18:14:17 -0500

    Create birthdayparadox.py
    Started o project #2. Started the getBirthday method.

---
+++ birthdayparadox.py
@@ -0,0 +1,21 @@
+"""Birthday Paradox Simulation, by Al Sweigart al@inventwithpython.com
+Explore the surprising probabilities of the "Birthday Paradox".
+More info at https://en.wikipedia.org/wiki/Birthday_problem
+View this code at https://nostarch.com/big-book-small-python-projects
+Tags: short, math, simulation"""
+
+import datetime, random
+
+def getBirthdays(numberOfBirthdays):
+    """Returns a list of number random date objects for birthdays."""
+    birthdays = []
+    for i in range(numberOfBirthdays):
+        # The year is unimportant for our simulation, as long as all
+        # birthdays have the same year.
+        startOfYear = datetime.date(2001, 1, 1)
+
+        # Get a random day into the year:
+        randomNumberOfDays = datetime.timedelta(random.randint(0, 364))
+        birthday = startOfYear + randomNumberOfDays
+        birthdays.append(birthday)
+    return birthdays
