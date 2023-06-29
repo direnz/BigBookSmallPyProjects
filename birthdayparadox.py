ca3a4cb2623516f91000d937a1a0d1b9d1be4552
direnc88 <direnc88@users.noreply.github.com>
2023-06-29 18:53:03 -0500

    Update birthdayparadox.py
    added the getMatch function.

--- birthdayparadox.py
+++ birthdayparadox.py
@@ -19,3 +19,16 @@ def getBirthdays(numberOfBirthdays):
         birthday = startOfYear + randomNumberOfDays
         birthdays.append(birthday)
     return birthdays
+
+def getMatch(birthdays):
+    """Returns the date object of a birthday that occurs more than once
+    in the birthdays list."""
+    if len(birthdays) == len(set(birthdays)):
+        return None # All birthdays are unique, so return None.
+
+    # Compare each birthday to every other birthday:
+    for a, birthdayA in enumerate(birthdays):
+        for b, birthdayB in enumerate(birthdays[a + 1 :]):
+            if birthdayA == birthdayB:
+                return birthdayA # Return the matching birthday.
+            
