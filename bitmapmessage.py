769a7669406ee284ce2d62f14c47c456a48f734c
direnc88 <direnc88@users.noreply.github.com>
2023-06-30 18:28:29 -0500

    Create bitmapmessage.py
    Started the program. Comments and imports.

---
+++ bitmapmessage.py
@@ -0,0 +1,14 @@
+"""Bitmap Message, by Al Sweigart al@inventwithpython.com
+Displays a text message according to the provided bitmap image.
+View this code at https://nostarch.com/big-book-small-python-projects
+Tags: tiny, beginner, artistic"""
+
+import sys
+
+# (!) Try changing this multiline string to any image you like:
+
+# There are 68 periods along the top and bottom of this string:
+# (You can also copy and paste this string from
+# https://inventwithpython.com/bitmapworld.txt)
+bitmap = """
+......................................................................
