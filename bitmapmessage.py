8f7f1d083094ddf6f4efd5c7f07fba0b112ade80
direnc88 <direnc88@users.noreply.github.com>
2023-06-30 19:23:30 -0500

    Update bitmapmessage.py
    Added double for loop and ifelse statement to loop over the bitmap.

--- bitmapmessage.py
+++ bitmapmessage.py
@@ -40,3 +40,14 @@ if message == '':
     sys.exit()
 
 # Loop over each line in the bitmap:
+for line in bitmap.splitlines():
+    # Loop over each character in the line:
+    for i, bit in enumerate(line):
+        if bit == ' ':
+            # Print an empty space since there's a space in the bitmap:
+            print(' ', end='')
+        else:
+            # Print a character from the message:
+            print(message[i % len(message)], end='')
+    print()  # Print a newline.
+
