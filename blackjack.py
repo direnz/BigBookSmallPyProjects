20ec95cfd8c0c2f2ea9dbd2db72f1ab2e987986e
direnc88 <direnc88@users.noreply.github.com>
2023-07-05 21:21:56 -0500

    Update blackjack.py
    Added the rules and started the main function.

--- blackjack.py
+++ blackjack.py
@@ -15,3 +15,28 @@ CLUBS    = chr(9827) # Character 9827 is '♣'.
 # (A list of chr codes is at https://inventwithpython.com/charactermap)
 BACKSIDE = 'backside'
 
+def main():
+    print('''Blackjack by Al Sweigart al@inventwithpython.com
+
+    Rules:
+        Try to get as close to 21 without going over.
+        Kings, Queens, and Jacks are worth 10 points.
+        Aces are worth 1 or 11 points.
+        Cards 2 through 10 are worth their face value.
+        (H)it to take another card.
+        (S)tand to stop taking cards.
+        On your first play, you can (D)ouble down to increase your bet
+        but must hit exactly one more time before standing.
+        In case of a tie, the bet is returned to the player.
+        The dealer stops hitting at 17.''')
+    
+    money = 5000
+    while True: # Main game loop.
+        # Check if the player has run out of money:
+        if money <= 0:
+            print("You're broke!")
+            print("Good thing you weren't playing with real money.")
+            print("Thanks for playing!")
+            sys.exit()
+
+        # Let the player enter their bet for this round:
