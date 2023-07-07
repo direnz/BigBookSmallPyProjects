dad541a7f888783151b82997afa286744c8d70fe
direnc88 <direnc88@users.noreply.github.com>
2023-07-07 17:22:32 -0500

    Update bouncingdvd.py
    Added direcitons, colors, and number of logos.

--- bouncingdvd.py
+++ bouncingdvd.py
@@ -15,3 +15,26 @@ except ImportError:
     print('following the instructions at https://pypi.org/project/Bext/')
     sys.exit()
 
+#Set up the constants:
+WIDTH, HEIGHT = bext.size()
+#We can't print to the last column on Windows without it adding a
+#newline automatically, so reduce the width by one:
+WIDTH -= 1
+
+NUMBER_OF_LOGOS = 5 #Try changing this to 1 or 100.
+PAUSE_AMOUNT = 0.2 #Try changing this to 0 or 1.0.
+#(!) Try changing DVDCORNER to a number between 0 and 3.
+COLORS = ['red', 'green', 'yellow', 'blue']
+
+UP_RIGHT    = 'ur'
+UP_LEFT     = 'ul'
+DOWN_RIGHT  = 'dr'
+DOWN_LEFT   = 'dl'
+DIRECTIONS  = (UP_RIGHT, UP_LEFT, DOWN_RIGHT, DOWN_LEFT)
+
+#Key names for logo dictionaries:
+COLOR = 'color'
+X = 'x'
+Y = 'y'
+DIR = 'direction'
+
