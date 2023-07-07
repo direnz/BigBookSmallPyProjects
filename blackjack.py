a62cf26f5ebe8713352cd1f42a27430e8ca94f42
direnc88 <direnc88@users.noreply.github.com>
2023-07-06 23:43:19 -0500

    Update blackjack.py
    added the getMove function and finished the program. Need to test.

--- blackjack.py
+++ blackjack.py
@@ -211,3 +211,30 @@ def displayCards(cards):
     # Print each row on the screen:
     for row in rows:
         print(row)
+
+def getMove(playerHand, money):
+    """Asks the player for their move, and returns 'H' for hit, 'S' for
+    stand, and 'D' for double down."""
+    while True: # Keep looping until the player enters a correct move.
+        # Determine what moves the player can make:
+        moves = ['(H)it', '(S)tand']
+
+        # The player can double down on their first move, which we can
+        # tell because they'll have exactly two cards:
+        if len(playerHand) == 2 and money > 0:
+            moves.append('(D)ouble down')
+
+        # Get the player's move:
+        movePrompt = ', '.join(moves) + '> '
+        move = input(movePrompt).upper()
+        if move in ('H', 'S'):
+            return move
+        if move == 'D' and '(D)ouble down' in moves:
+            return move
+
+        # If the player didn't enter a correct move, reprint the moves:
+        print('Enter one of', ', '.join(moves))
+
+# If the program is run (instead of imported), run the game:
+if __name__ == '__main__':
+    main()
