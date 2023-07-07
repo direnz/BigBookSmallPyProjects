2323a71da04132310c31cb8de99c107aa7cd2932
direnc88 <direnc88@users.noreply.github.com>
2023-07-07 18:09:34 -0500

    Update bouncingdvd.py
    Finished the main function. Added a counter to see if the logo bounces off of each corner. Added the ability for the logo to move. Display the number of corner bounces.

--- bouncingdvd.py
+++ bouncingdvd.py
@@ -38,3 +38,107 @@ X = 'x'
 Y = 'y'
 DIR = 'direction'
 
+def main():
+    bext.clear()
+
+    #Generate some logos.
+    logos = []
+    for i in range(NUMBER_OF_LOGOS):
+        logos.append({COLOR: random.choice(COLORS),
+                      X: random.randint(1, WIDTH - 4),
+                      Y: random.randint(1, HEIGHT - 4),
+                      DIR: random.choice(DIRECTIONS)})
+        if logos[-1][X] % 2 == 1:
+            #Make sure X is even so it can hit the corner.
+            logos[-1][X] -= 1
+
+    cornerBounces = 0 #Count how many times a logo hits a corner.
+    while True: #Main program loop.
+        for logo in logos: #Handle each logo in the logos list.
+            #Erase the logo's current location:
+            bext.goto(logo[X], logo[Y])
+            print('   ', end='') # (!) Try commenting this line out.
+
+            originalDirection = logo[DIR]
+
+            #See if the logo bounces off the corners:
+            if logo[X] == 0 and logo[Y] == 0:
+                logo[DIR] = DOWN_RIGHT
+                cornerBounces += 1
+            elif logo[X] == 0 and logo[Y] == HEIGHT - 1:
+                logo[DIR] = UP_RIGHT
+                cornerBounces += 1
+            elif logo[X] == WIDTH - 3 and logo[Y] == 0:
+                logo[DIR] = DOWN_LEFT
+                cornerBounces += 1
+            elif logo[X] == WIDTH - 3 and logo[Y] == HEIGHT - 1:
+                logo[DIR] = UP_LEFT
+                cornerBounces += 1
+
+            #See if the logo bounces off the left edge:
+            elif logo[X] == 0 and logo[DIR] == UP_LEFT:
+                logo[DIR] = UP_RIGHT
+            elif logo[X] == 0 and logo[DIR] == DOWN_LEFT:
+                logo[DIR] = DOWN_RIGHT
+
+            #See if the logo bounces off the right edge:
+            elif logo[X] == WIDTH - 3 and logo[DIR] == UP_RIGHT:
+                logo[DIR] = UP_LEFT
+            elif logo[X] == WIDTH - 3 and logo[DIR] == DOWN_RIGHT:
+                logo[DIR] = DOWN_LEFT
+
+            #See if the logo bounces off the top edge:
+            elif logo[Y] == 0 and logo[DIR] == UP_LEFT:
+                logo[DIR] = DOWN_LEFT
+            elif logo[Y] == 0 and logo[DIR] == UP_RIGHT:
+                logo[DIR] = DOWN_RIGHT
+
+            #See if the logo bounces off the bottom edge:
+            elif logo[Y] == HEIGHT - 1 and logo[DIR] == DOWN_LEFT:
+                logo[DIR] = UP_LEFT
+            elif logo[Y] == HEIGHT - 1 and logo[DIR] == DOWN_RIGHT:
+                logo[DIR] = UP_RIGHT
+
+            if logo[DIR] != originalDirection:
+                #Change color when the logo bounces:
+                logo[COLOR] = random.choice(COLORS)
+
+            #Move the logo. (X moves by 2 because the terminal
+            #characters are twice as tall as they are wide.)
+            if logo[DIR] == UP_RIGHT:
+                logo[X] += 2
+                logo[Y] -= 1
+            elif logo[DIR] == UP_LEFT:
+                logo[X] -= 2
+                logo[Y] -= 1
+            elif logo[DIR] == DOWN_RIGHT:
+                logo[X] += 2
+                logo[Y] += 1
+            elif logo[DIR] == DOWN_LEFT:
+                logo[X] -= 2
+                logo[Y] += 1
+
+        #Display number of corner bounces:
+        bext.goto(5, 0)
+        bext.fg('white')
+        print('Corner bounces:', cornerBounces, end='')
+
+        for logo in logos:
+            #Draw the logos at their new location:
+            bext.goto(logo[X], logo[Y])
+            bext.fg(logo[COLOR])
+            print('DVD', end='')
+
+        bext.goto(0, 0)
+
+        sys.stdout.flush() # (Required for bext-using programs.)
+        time.sleep(PAUSE_AMOUNT)
+
+#if this program was run (instead of imported), run the game:
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print()
+        print('Bouncing DVD Logo, by Al Sweigart')
+        sys.exit() #When Ctrl-C is pressed, end the program.
