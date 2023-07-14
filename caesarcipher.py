25ce1c4c0cb050514abaf545933e2f5e96b2fae1
direnc88 <direnc88@users.noreply.github.com>
2023-07-13 22:53:20 -0500

    Update caesarcipher.py
    finished the encrpyt/decrypt feature. added the ability for the user to enter a key to use. feature for user to enter message.

--- caesarcipher.py
+++ caesarcipher.py
@@ -16,3 +16,43 @@ except ImportError:
 SYMBOLS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 print('Caesar Cipher, by Al Sweigart')
+print('The Caesar cipher encrypts letters by shifting them over by a')
+print('key number. For example, a key of 2 means the letter A is')
+print('encrypted into C, the letter B encrypted into D, and so on.')
+print()
+
+#Let the user enter if they are encrypting or decrypting:
+while True: #Keep asking until the user enters e or d.
+    print('Do you want to (e)ncrypt or (d)ecrypt?')
+    response = input('> ').lower()
+    if response.startswith('e'):
+        mode = 'encrypt'
+        break
+    elif response.startswith('d'):
+        mode = 'decrypt'
+        break
+    print('Please enter the letter e or d.')
+
+#Let the user enter the key to use:
+while True: #Keep asking until the user enters a valid key.
+    maxKey = len(SYMBOLS) - 1
+    print('Please enter the key (0 to {})'.format(maxKey))
+    response = input('> ').upper()
+    if not response.isdecimal():
+        continue
+
+    if 0 <= int(response) <= len(SYMBOLS):
+        key = int(response)
+        break
+
+#Let the user enter the message to encrypt/decrypt:
+print('Enter the message to {}.'.format(mode))
+message = input('> ')
+
+# Caesar cipher only works on uppercase letters:
+message = message.upper()
+
+#Store the encrypted/decrypted form of the message:
+translated = ''
+
+#Encrypt/decrypt each symbol in the message:
