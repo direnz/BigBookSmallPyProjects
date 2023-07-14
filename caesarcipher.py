9df357f206281cfcffd052c852a4cf8ed1dade47
direnc88 <direnc88@users.noreply.github.com>
2023-07-13 23:02:08 -0500

    Update caesarcipher.py
    finished by adding the encrypt/decrypt each symbol in message in for loop.

--- caesarcipher.py
+++ caesarcipher.py
@@ -56,3 +56,33 @@ message = message.upper()
 translated = ''
 
 #Encrypt/decrypt each symbol in the message:
+for symbol in message:
+    if symbol in SYMBOLS:
+        #Get the encrypted (or decrypted) number for this symbol.
+        num = SYMBOLS.find(symbol) #Get the number of the symbol.
+        if mode == 'encrypt':
+            num = num + key
+        elif mode == 'decrypt':
+            num = num - key
+
+        #Handle the wrap-around if num is larger than the length of
+        #SYMBOLS or less than 0:
+        if num >= len(SYMBOLS):
+            num = num - len(SYMBOLS)
+        elif num < 0:
+            num = num + len(SYMBOLS)
+
+        #Add encrypted/decrypted number's symbol at the end of translated:
+        translated = translated + SYMBOLS[num]
+    else:
+        #Just add the symbol without encrypting/decrypting:
+        translated = translated + symbol
+
+#Display the encrypted/decrypted string to the screen:
+print(translated)
+
+try:
+    pyperclip.copy(translated)
+    print('Full {}ed text copied to clipboard.'.format(mode))
+except:
+    pass #Do nothing if pyperclip wasn't installed.
