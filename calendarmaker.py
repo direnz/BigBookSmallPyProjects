fc09461933b734b712c0f91771cb2c5ed5c156c6
direnc88 <direnc88@users.noreply.github.com>
2023-07-27 21:25:28 -0500

    Create calendarmaker.py
    started the program

---
+++ calendarmaker.py
@@ -0,0 +1,6 @@
+"""Calendar Maker, by Al Sweigart al@inventwithpython.com
+Create monthly calendars, saved to a text file and fit for printing.
+View this code at https://nostarch.com/big-book-small-python-programming
+Tags: short"""
+
+import datetime
