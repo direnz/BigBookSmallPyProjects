3e602d1348e6ec216e1dbfa2d10d6803aeb0adb6
direnc88 <direnc88@users.noreply.github.com>
2023-07-27 21:30:33 -0500

    Update calendarmaker.py
    Added variables and while true statement to get year from user.

--- calendarmaker.py
+++ calendarmaker.py
@@ -4,3 +4,20 @@ View this code at https://nostarch.com/big-book-small-python-programming
 Tags: short"""
 
 import datetime
+
+# Set up the constants:
+DAYS = ('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday')
+
+MONTHS = ('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September','October', 'November', 'December')
+
+print('Calendar Maker, by Al Sweigart al@inventwithpython.com')
+
+while True:  # Loop to get a year from the user.
+    print('Enter the year for the calendar:')
+    response = input('> ')
+
+    if response.isdecimal() and int(response) > 0:
+        year = int(response)
+        break
+    print('Please enter a numeric year, like 2023.')
+
