eb2d5e1d8318b1786feca4fd1223a34095636575
direnz <charlesdirenzo@gmail.com>
2023-08-25 18:13:05 -0500

    Create chohan.py

---
+++ chohan.py
@@ -0,0 +1,7 @@
+"""Cho-Han, by Al Sweigart al@inventwithpython.com
+The traditional Japanese dice game of even-odd. 
+view this code at https://nostarch.com/big-book-small-python-projects
+Tags: short, beginner, game"""
+
+import random, sys
+
