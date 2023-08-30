8cd99b9635bf84529785e845acc212acec9b1ccd
direnz <charlesdirenzo@gmail.com>
2023-08-29 20:33:05 -0500

    Update collatz.py

--- collatz.py
+++ collatz.py
@@ -17,3 +17,22 @@ print('''Collatz Sequence, or, the 3n + 1 Problem
       
       it is generally thought, but so far not mathematically proven, that every starting number eventually terminates at 1.''')
 
+print('Enter a starting number (greater than 0) or QUIT:')
+response = input('> ')
+
+if not response.isdecimal() or response == '0':
+    print('Please enter a number greater than 0.')
+    sys.exit()
+
+n = int(response)
+while n != 1:
+    if n % 2 == 0: # If n is even...
+        n = n // 2
+    else: # If n is odd...
+        n = n * 3 + 1
+    time.sleep(0.1) # Add a slight delay.
+
+    print(', ' + str(n), end='', flush=True) # Display the sequence.
+    time.sleep(0.1) #adding a delay
+
+print()
