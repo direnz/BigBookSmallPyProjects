c1925dc08be782bfc43ded8813a593d072f64078
direnz <charlesdirenzo@gmail.com>
2023-08-30 18:40:09 -0500

    Update conwaysgameoflife.py

--- conwaysgameoflife.py
+++ conwaysgameoflife.py
@@ -16,5 +16,75 @@ DEAD = ' ' # The character representing a dead cell.
 
 #(!) Try changing ALIVE to '|' and DEAD to '-'.
 
+# The cells and nextCells are dictionaries for the state of the game.
+# Their keys are (x, y) tuples and their values are one of the ALIVE
+# or DEAD values.
+nextCells = {}
+# Put random dead and alive cells into nextCells:
+for x in range(WIDTH): # Loop over every possible column.
+    for y in range(HEIGHT): # Loop over every possible row.
+        # 50/50 chance for starting cells being alive or dead.
+        if random.randint(0, 1) == 0:
+            nextCells[(x, y)] = ALIVE # add a living cell
+        else:
+            nextCells[(x, y)] = DEAD  #add a dead cell
 
+while True: # Main program loop.
+    # Each iteration of this loop is a step of the simulation.
+
+    print('\n' * 50) # Separate each step with newlines.
+    cells = copy.deepcopy(nextCells)
+
+    # Print cells on the screen:
+    for y in range(HEIGHT):
+        for x in range(WIDTH):
+            print(cells[(x, y)], end='') # Print the # or space.
+        print() # Print a newline at the end of the row.
+    print('Press Ctrl-C to quit.')
+
+    # Calculate the next step's cells based on current step's cells:
+    for x in range(WIDTH):
+        for y in range(HEIGHT):
+            # Get neighboring coordinates of (x, y ), even if they wrap around the edge:
+            left = (x - 1) % WIDTH
+            right = (x + 1) % WIDTH
+            above = (y - 1) % HEIGHT
+            below = (y + 1) % HEIGHT
+
+            # Count number of living neighbors:
+            numNeighbors = 0
+            if cells[(left, above)] == ALIVE:
+                numNeighbors += 1 # Top-left neighbor is alive.
+            if cells[(x, above)] == ALIVE:
+                numNeighbors += 1 # Top neighbor is alive.
+            if cells[(right, above)] == ALIVE:
+                numNeighbors += 1 # Top-right neighbor is alive.
+            if cells[(left, y)] == ALIVE:
+                numNeighbors += 1 # Left neighbor is alive.
+            if cells[(right, y)] == ALIVE:
+                numNeighbors += 1 # Right neighbor is alive.
+            if cells[(left, below)] == ALIVE:
+                numNeighbors += 1 # Bottom-left neighbor is alive.
+            if cells[(x, below)] == ALIVE:
+                numNeighbors += 1 # Bottom neighbor is alive.
+            if cells[(right, below)] == ALIVE:
+                numNeighbors += 1 # Bottom-right neighbor is alive.
+
+            # Set cell based on Conway's Game of Life rules:
+            if cells[(x, y)] == ALIVE and (numNeighbors == 2 or numNeighbors == 3):
+                # Living cells with 2 or 3 neighbors stay alive:
+                nextCells[(x, y)] = ALIVE
+            elif cells[(x, y)] == DEAD and numNeighbors == 3:
+                # Dead cells with 3 neighbors become alive:
+                nextCells[(x, y)] = ALIVE
+            else:
+                # Everything else dies or stays dead:
+                nextCells[(x, y)] = DEAD
+
+    try:
+        time.sleep(1) # Add a 1-second pause to reduce flickering.
+    except KeyboardInterrupt:
+        print('Conway\'s Game of Life')
+        print('By Al Sweigart al@inventwithpyhon.com')
+        sys.exit() # When Ctrl-C is pressed, end the program.
 
